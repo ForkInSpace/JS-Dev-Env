81a1e9adffccaffa922a40add7b7b3956ba69fc1
Forkinspace <ts.zaza@gmail.com>
2017-09-10 18:01:43 +0300

    Dom test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import numeral from 'numeral';
-import 'bugsnag-3.min.js';
 import './index.css';
 
 /* eslint-disable no-console */
 
 const courseValue = numeral(1000).format('$0,0.00');
-
-Bugsnag.notify("Syntax err", "Syntax err");
 
 console.log('I would pay ' + courseValue + ' for this awesome course!');
 
