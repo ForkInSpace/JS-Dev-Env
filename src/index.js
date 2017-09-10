98c2a864498c070d46469e79f1d06f737305ad8b
Forkinspace <ts.zaza@gmail.com>
2017-09-10 10:54:49 +0300

    Bugsnag snippet removed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import numeral from 'numeral';
+import 'bugsnag-3.min.js';
 import './index.css';
 
 /* eslint-disable no-console */
 
 const courseValue = numeral(1000).format('$0,0.00');
 
+Bugsnag.notify("Syntax err", "Syntax err");
+
 console.log('I would pay ' + courseValue + ' for this awesome course!');
+
